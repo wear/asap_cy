b295dac49f8cf81621a7b8a70a6d756898def29a
wear <wear63659220@gmail.com>
2009-07-17 17:34:12 +0800

    fix

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -27,26 +27,6 @@ function add_dish(dish) {
   $('dishes').value +=  ' ' + dish;      	
 } 
 
-function send_verify(mobile) { 
-  var reg0=/^13\d{5,9}$/;   //130--139。至少7位
-  var reg1=/^153\d{8}$/;  //联通153。至少7位
-  var reg2=/^159\d{8}$/;  //移动159。至少7位
-  var reg3=/^158\d{8}$/;
-  var reg4=/^150\d{8}$/;
-  var my=false;
-  if (reg0.test(mobile))my=true;
-  if (reg1.test(mobile))my=true;
-  if (reg2.test(mobile))my=true;
-  if (reg3.test(mobile))my=true;
-  if (reg4.test(mobile))my=true;
-	 if (!my){
-		alert('对不起，您输入的手机号码错误。');
-	 }else{
-		$.get("/send_verify_code/" + mobile, function(data){
-		  alert("发送结果: " + data);
-		});
-	 }  	
-}
 
 function mark_for_destroy(element) { 
   $(element).next('.should_destroy').value = 1 
